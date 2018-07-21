c7b8f8a0f9a2731b5276c2230a8d949b8a85acd5
tony <tonyofbyteball@users.noreply.github.com>
2018-07-21 13:47:19 +0300

    remove price from greeting

--- modules/texts.js
+++ modules/texts.js
@@ -15,8 +15,7 @@ exports.greeting = () => {
 		"In the latter case, only a proof of attestation will be posted publicly on the distributed ledger. ",
 		"\n\n",
 
-		`The price of attestation is ${conf.priceInBytes/1e9} GB. `,
-		"The payment is nonrefundable even if the attestation fails for any reason.\n\n",
+		conf.bAllowProofByPayment ? `The price of attestation is ${conf.priceInBytes/1e9} GB.  The payment is nonrefundable even if the attestation fails for any reason.\n\n` : '',
 
 		`After you successfully attest your steem username for the first time, `,
 		`you receive a reward in Bytes that depends on your reputation in Steem:\n\n${rewards}\n\nHalf of the reward will be immediately available, the other half will be locked on a smart contract and can be spent after 1 year.`
