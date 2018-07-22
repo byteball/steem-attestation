d7bd03689f87200fb67704d8b12ca10381d5485e
Tarmo Annus <tarmo888@hotmail.com>
2018-07-22 12:19:02 +0300

    qr endpoint

--- attestation.js
+++ attestation.js
@@ -79,6 +79,10 @@ function startWebServer(){
 		});
 	});
 
+	app.get('/qr', (req, res) => {
+		res.sendFile(__dirname+'/qr.html');
+	});
+
 	server.listen(conf.webPort, () => {
 		console.log(`== server started listening on ${conf.webPort} port`);
 	});
