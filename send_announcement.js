f8aba0a61e8ca8a0443fea33a7eebb51d109e12c
tony <tonyofbyteball@users.noreply.github.com>
2018-08-29 00:10:33 +0200

    send announcement

---
+++ send_announcement.js
@@ -0,0 +1,70 @@
+/*jslint node: true */
+"use strict";
+var async = require('byteballcore/node_modules/async');
+var db = require('byteballcore/db.js');
+var eventBus = require('byteballcore/event_bus.js');
+var headlessWallet = require('headless-byteball');
+
+
+const announcement = `Dear Steemians!
+
+Byteball is thrilled to be featured in Binance's monthly community coin vote.
+If you have an account on Binance, please vote to support the Byteball platform https://www.binance.com/vote.html
+
+Being listed on Binance helps attract attention to projects. It might potentially benefit you and the value of the Bytes you have on smart contracts. The deadline to vote is August 31. We currently have around 160 votes and your vote is really important. 
+
+Thank you Steemians!
+
+Team Byteball
+
+https://www.binance.com/vote.html
+`;
+//const optout_text = "\n\nIf you don't want to receive news here, [click here to opt out](command:optout).";
+const message = announcement;// + optout_text;
+
+headlessWallet.setupChatEventHandlers();
+
+function sendAnnouncement(){
+	var device = require('byteballcore/device.js');
+	db.query(
+		"SELECT device_address FROM users",
+		rows => {
+			console.error(rows.length+" messages will be sent");
+			async.eachSeries(
+				rows,
+				(row, cb) => {
+					device.sendMessageToDevice(row.device_address, 'text', message, {
+						ifOk: function(){}, 
+						ifError: function(){}, 
+						onSaved: function(){
+							console.log("sent to "+row.device_address);
+							cb();
+						}
+					});
+				},
+				() => {
+					console.error("=== done");
+				}
+			);
+		}
+	);
+}
+
+eventBus.on('text', function(from_address, text){
+	var device = require('byteballcore/device.js');
+	console.log('text from '+from_address+': '+text);
+	text = text.trim().toLowerCase();
+	/*if (text === 'optout'){
+		db.query("INSERT "+db.getIgnore()+" INTO optouts (device_address) VALUES(?)", [from_address]);
+		return device.sendMessageToDevice(from_address, 'text', 'You are unsubscribed from future announcements.');
+	}
+	else */if (text.match(/thank/))
+		device.sendMessageToDevice(from_address, 'text', "You're welcome!");
+	else
+		device.sendMessageToDevice(from_address, 'text', "Usual operations are paused while sending announcements.  Check again in a few minutes.");
+});
+
+eventBus.on('headless_wallet_ready', () => {
+	setTimeout(sendAnnouncement, 1000);
+});
+
