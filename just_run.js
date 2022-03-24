4fb4661d6b629565e5c822f888c212bbc04f9fab
tony <tonyofbyteball@users.noreply.github.com>
2022-03-24 23:23:35 +0300

    consolidate after catchup

--- just_run.js
+++ just_run.js
@@ -3,13 +3,15 @@
 
 var headlessWallet = require('headless-obyte');
 const eventBus = require('ocore/event_bus');
+const network = require('ocore/network');
 
 
 async function consolidate() {
 	const address = await headlessWallet.issueOrSelectAddressByIndex(0, 1);
-
+	await network.waitUntilCatchedUp();
+	console.log('catched up, will consolidate');
 	const consolidation = require('headless-obyte/consolidation.js');
 	consolidation.consolidate(address, headlessWallet.signer, 1);		
 }
 
-eventBus.once('headless_wallet_ready', () => setTimeout(consolidate, 60 * 1000));
+eventBus.once('headless_wallet_ready', consolidate);
