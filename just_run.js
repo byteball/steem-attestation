79a0efaa890ddc140b66f1565518ae8a1d16b010
tony <tonyofbyteball@users.noreply.github.com>
2022-03-24 22:54:18 +0300

    consolidate

--- just_run.js
+++ just_run.js
@@ -2,6 +2,14 @@
 "use strict";
 
 var headlessWallet = require('headless-obyte');
+const eventBus = require('ocore/event_bus');
 
 
+async function consolidate() {
+	const address = await headlessWallet.issueOrSelectAddressByIndex(0, 1);
 
+	const consolidation = require('headless-obyte/consolidation.js');
+	consolidation.consolidate(address, headlessWallet.signer, 1);		
+}
+
+eventBus.once('headless_wallet_ready', () => setTimeout(consolidate, 60 * 1000));
